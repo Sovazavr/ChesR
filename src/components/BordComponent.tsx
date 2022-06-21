7e55111fe5d107e881e1b9001f3630fd0df66e11
sovazavr <vadimsopov00@mail.ru>
2022-06-21 13:30:39 +0700

    addFigure method

---
+++ src/components/BordComponent.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from 'react'
+import { Board } from '../modules/Board';
+import CellComponent from './CellComponent'
+
+
+interface BoardProps {
+    board: Board;
+    setBoard: (board: Board) => void;
+
+}
+
+const BordComponent: FC<BoardProps> = ({ board, setBoard }) => {
+    return (
+        <div className="board">
+            {board.cells.map((row, index) =>
+                <React.Fragment key={index}>
+                    {row.map(cell =>
+                        <CellComponent
+                            cell={cell}
+                            key={cell.id}
+                        />
+                    )}
+                </React.Fragment>
+            )}
+        </div>
+    )
+}
+
+export default BordComponent
